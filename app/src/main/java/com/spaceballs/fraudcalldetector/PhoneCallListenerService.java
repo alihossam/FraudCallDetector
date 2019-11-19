60da258361d2e29975044bca98e2f0df50977392
Ali Mohamed <alihossam_10@live.com>
2019-11-18 23:02:18 -0800

    Call converter after stopping recorder

--- app/src/main/java/com/spaceballs/fraudcalldetector/PhoneCallListenerService.java
+++ app/src/main/java/com/spaceballs/fraudcalldetector/PhoneCallListenerService.java
@@ -44,6 +44,7 @@ public class PhoneCallListenerService extends Service {
                     recorder.stop();
                     //STTService.speechToTextUsingGoogle(new File(recorder.getSavedFileAbsolutePath()));
                     //STTService.convertToFlac(recorder.getSavedFileAbsolutePath(), getApplicationContext());
+                    STTService.convertToFlac(recorder.getSavedFileAbsolutePath(), getApplicationContext());
                 }
                 else if(state == TelephonyManager.CALL_STATE_OFFHOOK) {
                     prevState = TelephonyManager.CALL_STATE_OFFHOOK;
@@ -51,7 +52,7 @@ public class PhoneCallListenerService extends Service {
                     recorder.start();
                 } else if(state == TelephonyManager.CALL_STATE_IDLE) {
                     //STTService.speechToTextUsingGoogle(new File(recorder.getSavedFileAbsolutePath()));
-                    STTService.convertToFlac(recorder.getSavedFileAbsolutePath(), getApplicationContext());
+                    //STTService.convertToFlac(recorder.getSavedFileAbsolutePath(), getApplicationContext());
                 }
 
             }
